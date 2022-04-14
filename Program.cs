5c03f7bc52579cc5d1c219dff3bcc1ad25e81322
Shane Esplin <Shane.Esplin@snow.edu>
2022-04-14 10:13:36 -0600

    Added user greeting

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Shane Esplin, CS 1405 Final
+
+using System.Diagnostics;
+
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello. This program helps a DM to make a random encounter for their group.");
+        Console.WriteLine("If you enter in the number of players in thegroup, the average ecl of the group, and the environment the group is in,");
+        Console.WriteLine("this program will give you an encounter based on the difficulty you desire for the encounter.");
+
+        
+    }
+
+    static void AddMonstertoList()
+    {
+
+    }
+}
