a51c09c12aa4b7c03f6959e411a8a283024aa96b
Shane Esplin <Shane.Esplin@snow.edu>
2022-04-28 17:13:07 -0600

    Working Main Menu

--- RandomEncounterGenerator.cs
+++ RandomEncounterGenerator.cs
@@ -3,44 +3,94 @@
 using System.Diagnostics;
 using System;
 
-class program
+class RandomEncounterGenerator
 {
     static void Main()
     {
+        int players = 0;
+        double averageECL = 0.0;
+        string environment = "All";
+        string difficulty = "Normal";
+        int userInput = 0;
+
+        Console.Clear();
+        //User Greeting
         Console.WriteLine("Hello. This program helps a DM to make a random encounter for their group.");
         Console.WriteLine("If you enter in the number of players in the group, the average ecl of the group, and the environment the group is in,");
         Console.WriteLine("this program will give you an encounter based on the difficulty you desire for the encounter.");
         Console.WriteLine("");
         Console.WriteLine("");
+        Console.WriteLine("Press and key to begin.");
 
-        ConsoleKey userInput;
-        char charInput;
+        Console.ReadKey();
 
         do
         {
+            Console.Clear();
+            Console.WriteLine("Players: " + players);
+            Console.WriteLine("Average Player ECL: " + averageECL);
+            Console.WriteLine("Environmnet: " + environment);
+            Console.WriteLine("Difficulty: " + difficulty);
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Please choose an option:");
-            Console.WriteLine("A) Enter the number of players");
-            Console.WriteLine("B) Enter the average ECL of the players");
-            Console.WriteLine("C) Choose an environment.");
-            Console.WriteLine("D) Create an encounter.");
+            Console.WriteLine("1) Enter the number of players (0 by default).");
+            Console.WriteLine("2) Enter the average ECL of the players (0 by default).");
+            Console.WriteLine("3) Choose an environment ('All' by default).");
+            Console.WriteLine("4) Set the difficulty ('Normal' by default).");
+            Console.WriteLine("5) Create an encounter (must have a valid value in player number and average ECL).");
+            Console.WriteLine("0) Exit the program.");
 
-            userInput = Console.ReadKey(true).Key;
-            charInput = (char)userInput;
+            userInput = int.Parse(Console.ReadLine());
 
-            switch(charInput)
+            switch(userInput)
             {
-                case A:
-                {
-                    break;
-                }
-                case B:
-                {
-                    break;
-                }
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Test Case 1");
 
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    break;
+                
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Test Case 2");
+
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    break;
+                
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Test Case 3");
+
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    break;
+                
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Test Case 4");
+
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    break;
+                
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("Test Case 5");
+
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                    break;                
             }
 
-        }while(userInput != ConsoleKey.Escape);
+        }while(userInput != 0);
+
+        Console.Clear();
+        Console.WriteLine("");
+        Console.WriteLine("Thank you for using this program.");
     }
 
     static void CreatEncounter()
@@ -48,5 +98,18 @@ class program
 
     }
 
+    static void PlayerNumberEntry()
+    {
 
+    }
+
+    static void AverageECLEntry()
+    {
+
+    }
+
+    static void EnvironmentEntry()
+    {
+
+    }
 }
